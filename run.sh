b854fc360a943b79291bcd65f9ab75b070e1837e
choman <choman@localhost.localdomain>
2017-02-02 14:44:56 -0600

    Got sound and smartcards working

--- run.sh
+++ run.sh
@@ -14,23 +14,28 @@ if [ -n "$1" ]; then
 fi
 
 
+        ##--device=/dev/bus/usb/003/003 \
+##        --device=/dev/tty/USB0 \
 if [ -n "$myfile" ]; then
     docker run -it --rm -e DISPLAY=$DISPLAY \
+        --privileged \
         -v $HOME/Desktop:/root/Desktop \
         -v $HOME/Downloads:/root/Downloads \
         -v $cwd/files:/root/Files \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /dev/snd:/dev/snd \
-        --privileged \
+        -v /dev/bus/usb/003/003:/dev/bus/usb/003/003 \
         rdp "$myfile"
 else
     docker run -it --rm -e DISPLAY=$DISPLAY \
+        -e PULSE_SERVER=192.168.2.100:4713 \
+        --privileged \
         -v $HOME/Desktop:/root/Desktop \
         -v $HOME/Downloads:/root/Downloads \
         -v $cwd/files:/root/Files \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /dev/snd:/dev/snd \
-        --privileged \
+        -v /dev/bus/usb/003/003:/dev/bus/usb/003/003 \
         rdp
 fi
 
